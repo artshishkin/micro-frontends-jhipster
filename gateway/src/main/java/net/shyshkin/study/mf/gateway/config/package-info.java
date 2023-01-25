ed4524e80c0af4bf80c0bd32b480185cbd2abab0
Art <d.art.shishkin@gmail.com>
2023-01-25 13:08:18 +0200

    jhipster jdl reactive-mf.jdl --monorepository --workspaces

---
+++ gateway/src/main/java/net/shyshkin/study/mf/gateway/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package net.shyshkin.study.mf.gateway.config;
---
+++ gateway/src/main/java/net/shyshkin/study/mf/gateway/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package net.shyshkin.study.mf.gateway.service.dto;
